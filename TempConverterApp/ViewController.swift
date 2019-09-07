d209106abf43166895d874af82a37f0b35a652e5
David Inyangson <dinyangs@andrew.cmu.edu>
2019-09-07 14:15:05 -0400

    Initial Commit

---
+++ TempConverterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TempConverterApp
+//
+//  Created by David Inyangson on 9/7/19.
+//  Copyright © 2019 David Inyangson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
